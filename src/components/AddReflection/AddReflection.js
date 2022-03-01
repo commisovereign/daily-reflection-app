b0c56e12ebd708ab778ae46546412a7f93da9076
commisovereign <nolle044@umn.edu>
2022-03-01 17:51:27 -0600

    date and select update 3/1/2022

--- src/components/AddReflection/AddReflection.js
+++ src/components/AddReflection/AddReflection.js
@@ -1,9 +1,11 @@
 import {useState} from 'react';
-import Radio from './Radio';
+import DatePicker from 'react-date-picker';
 
 const AddReflection = ({onAdd}) => {
+  const [value,onChange] = useState(new Date())
   const [productivity, setProductivity] = useState(0)
-  const [notes, setNotes] = useState("")
+  const [notes, setNotes] = useState('')
+  const [dayScore,setDayScore] = useState(0)
 
   const onSubmit = (e) => {
     e.preventDefault()
@@ -12,19 +14,42 @@ const AddReflection = ({onAdd}) => {
       return
     }
     
-    onAdd({notes,productivity})
+    onAdd({dayScore,productivity,notes})
 
 
     setProductivity(0)
-    setNotes("")
+    setNotes('')
+    setDayScore(1)
   
   }
   return(
     <form className='add-reflection' onSubmit={onSubmit}>
-      <Radio/>
+      <DatePicker onChange={onChange} value ={value} maxDate= {new Date()}/>
+      <div>
+      How did you feel today (1-5)?
+      <select value={dayScore} onChange={(e)=> setDayScore(e.target.value)}>
+        <option value = {1}>1</option>
+        <option value = {2}>2</option>
+        <option value = {3}>3</option>
+        <option value = {4}>4</option>
+        <option value = {5}>5</option>
+      </select>
+      </div>
+      <br/>
+      <div>
+      How Productive were you (1-5)?
+      <select value={productivity} onChange={(e)=> setProductivity(e.target.value)}>
+        <option value = {1}>1</option>
+        <option value = {2}>2</option>
+        <option value = {3}>3</option>
+        <option value = {4}>4</option>
+        <option value = {5}>5</option>
+      </select>
+      </div>
       <div>
         <input type= 'text'
         placeholder='comments:'
+        value={notes}
         onChange ={(e)=> setNotes(e.target.value)}/>
       </div>
       <input type='submit' value='Save Day'/>
