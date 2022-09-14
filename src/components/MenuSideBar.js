e0f05040978f8a67f16538718af2dda199421ad7
commisovereign <zaidnolley@gmail.com>
2022-09-14 15:36:56 -0500

    9/14/2022 Fixed about link issues and sorted graphs

---
+++ src/components/MenuSideBar.js
@@ -0,0 +1,15 @@
+
+
+const MenuSideBar = ({}) => {
+
+
+    return(
+        <div className="side-menu">
+            <a>Close</a><br/>
+            <a>Link 1</a><br/>
+            <a>button 1</a><br/>
+            <a>button 2</a>
+        </div>
+    )
+};
+export default MenuSideBar;
